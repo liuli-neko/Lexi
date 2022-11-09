db32903a598f0025015a4bc04133ad7f6d248001
llhsdmd <2982886437@qq.com>
2022-11-09 23:30:42 +0800

    fix

--- core/window/window.hpp
+++ core/window/window.hpp
@@ -20,7 +20,7 @@ public:
   auto DrawImg(const Mat &img, const Rectd &rect) const -> void;
   auto DrawLine(const Pointd &x, const Pointd &y, const Color &color,
                 const int thickness = 1,
-                const LineType &line_type = SOLID_LINE) const -> void;
+                const LineType &line_type = LineType::Solid) const -> void;
   auto SetPainterColor(const Color &color) -> void;
 
 private:
