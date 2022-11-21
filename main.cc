6e33709814701a0f7f0aa045afdf0ef9f1ba0cf7
琉璃花社的猫朵 <2982886437@qq.com>
2022-11-21 09:09:27 +0000

    new a window

--- main.cc
+++ main.cc
@@ -1,10 +1,35 @@
-#include <iostream>
 #include <Btk/comctl.hpp>
 #include <Btk/context.hpp>
+#include <iostream>
 
+#include "Btk/widget.hpp"
+#include "Btk/widgets/button.hpp"
 #include "common/log/lexi_log.hpp"
+
+class Window : public Btk::Widget {
+public:
+  Window() {
+    btn.signal_clicked().connect([this, n = 0]() mutable {
+      n += 1;
+      if (n == 5) {
+        this->close();
+      }
+    });
+
+    // Or add widget like this
+    auto label = new Btk::Label("HelloWorld");
+    add_child(label);
+  }
+
+private:
+  Btk::Button btn{this};
+};
 
 int main() {
   LOG_INFO("this is a log %d", 1024);
+  Btk::UIContext ctxt;
+  Window win;
+  win.show();
+  ctxt.run();
   return 0;
 }
