2aa8f867fb3b308fc9f63f342303b17f7d3ad5bf
llhsdmd <2982886437@qq.com>
2022-11-21 21:46:07 +0800

    update log

--- common/log/lexi_log.hpp
+++ common/log/lexi_log.hpp
@@ -4,20 +4,21 @@
 
 #include <chrono>
 
-#define LOG_INFO(fmt, ...)                                               \
-  printf("INFO - [%s:%d] " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__); \
+#define LOG_INFO(fmt, ...)                                                     \
+  printf("INFO - [%s:%d] " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__);       \
   void(0)
 
-#define LOG_WARN(fmt, ...)                                               \
-  printf("WARN - [%s:%d] " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__); \
+#define LOG_WARN(fmt, ...)                                                     \
+  printf("WARN - [%s:%d] " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__);       \
   void(0)
 
-#define LOG_ERROR(fmt, ...)                                               \
-  printf("ERROR - [%s:%d] " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__); \
+#define LOG_ERROR(fmt, ...)                                                    \
+  printf("ERROR - [%s:%d] " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__);      \
   exit(0)
 
-#define ASSERT(cond, ...)     \
-  if (!(cond)) {              \
-    LOG_ERROR(##__VA_ARGS__); \
-  }                           \
+#define ASSERT(cond, fmt, ...)                                                 \
+  if (!(cond)) {                                                               \
+    printf("ASSERT - [%s:%d] " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__);   \
+    exit(1);                                                                   \
+  }                                                                            \
   void(0)
